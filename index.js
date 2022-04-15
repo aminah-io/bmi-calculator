eb8da9abd32360612cc3f0136078626e6e121566
Aminah Burch <hello@aminah.io>
2022-04-15 14:26:17 +0300

    Updated the server file to make port variable and added a start script to package json

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
